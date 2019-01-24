1b04693700d8ebfd0e87ef59abfbf803911c35d1
inari a <iaaltojarvi@gmail.com>
2019-01-24 15:14:45 +0100

    kommittaan prkl

---
+++ script.js
@@ -0,0 +1,45 @@
+
+const listDiv = document.querySelector('.list');
+const descriptionInput = document.querySelector('input.description');
+const descriptionP = document.querySelector('p.description');
+const descriptionButton = document.querySelector('button.description');
+const listUl = listDiv.querySelector('ul');
+const addItemInput = document.querySelector('input.addItemInput');
+const addItemButton = document.querySelector('button.addItemButton');
+const removeItemButton = document.querySelector('button.removeItemButton');
+const lis = listUl.children;
+
+
+descriptionButton.addEventListener('click', () => {
+  descriptionP.innerHTML = descriptionInput.value + ':';
+  descriptionInput.value = '';
+});
+
+addItemButton.addEventListener('click', () => {
+  let ul = document.getElementsByTagName('ul')[0];
+  let li = document.createElement('li');
+  li.textContent = addItemInput.value;
+  ul.appendChild(li);
+  addItemInput.value = '';
+});
+
+function newItem() {
+    var item = document.getElementById("input").value;
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(" " + item));
+    ul.appendChild(li);
+    document.getElementById("input").value = "";
+    li.onclick = removeItem;
+  }
+  
+  document.body.onkeyup = function(e) {
+    if (e.keyCode == 13) {
+      newItem();
+    }
+  };
+  
+  function removeItem(e) {
+    e.target.parentElement.removeChild(e.target);
+  }
+
